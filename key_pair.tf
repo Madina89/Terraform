16187e0742505db18d7e4c3cbfe34e83fb308ae7
farrukh90 <farrukhsadykov@gmail.com>
2019-07-28 16:25:13 -0500

    added proper key

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "terraform_april" {
   key_name   = "terraform_april"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLSNKiXBF2K0u6hTkSnOiX/IbmAmJM/fm4GJb1pIbfT3jpEM6e5uD7wbBPs8J1s0S5iTdDmVQ+K9WtTxMeqLjQCOodp8VY301GOlZoTw+Pk8satUvrbDypirbSGOKnMFPc+3mg97DOtyQAu3g9aTn2DZUlj/s2V8YFE/BoZrQlM/D6BZeJpWg552I9hJKJ9JJYB+5KtazfK3f3IPMHuQbVcy5DiFXyN/3R3v/urvo109WZZK0NnBWijoSpuinbWg+hTD4v7jctYU2C+fo6HSzy+9LZNUrpv6ElwKfeb6YUeiyT2aUofuIM4hijk0KyJgVfXxGjMD2RW6q1dCK5Oxi7 root@ip-172-31-29-167.eu-west-1.compute.internal"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
