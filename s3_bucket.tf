4b5eea305e56b8ca225676f729a4fee1124da031
Madina89 <madinalinux89@gmail.com>
2019-08-03 07:35:36 -0500

    s3 bucket

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-farrukh"
+  bucket = "my-tf-test-bucket-madina"
   acl    = "private"
 
   tags = {
         Name = "Dev"
         Dept =  "IT"
         Group = "April"
-        Created_by = "Farrukh"
+        Created_by = "Madina"
     }
 }
