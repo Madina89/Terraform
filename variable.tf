b94d1418dbe7d560a7aef24749a9a9fe87e4ec36
Madina89 <madinalinux89@gmail.com>
2019-08-03 09:01:15 -0500

    variable added

--- variable.tf
+++ variable.tf
@@ -1,3 +1,12 @@
 variable "ami" {}
 variable "instance_type" {}
 variable "Env" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "ami" {}
+variable "vpc_id" {}
+variable "user" {}
+variable "ssh_key_location" {}
+variable "zone_id" {}
+variable "domain" {}
+variable "region" {}
